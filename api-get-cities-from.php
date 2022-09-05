cc718414e29a2e1160ac28380080b020122620fe
filip <fso@stromlin.com>
2022-09-05 08:39:15 +0200

    change cities from api

--- api-get-cities-from.php
+++ api-get-cities-from.php
@@ -2,23 +2,23 @@
 
 $cities = [
     [
-        'city_name' => 'Copenhagen',
-        'city_airport' => 'CPH Kastrup',
-        'city_image' => 'city-thumbnail-01.png',
+        'city' => 'Copenhagen',
+        'name' => 'CPH Kastrup',
+        'image' => 'copenhagen.png',
         'city_population' => '1.000'
 
 
     ],
     [
-        'city_name' => 'Turku',
-        'city_airport' => 'Turun lentoasema',
-        'city_image' => 'city-thumbnail-02.png',
+        'city' => 'Turku',
+        'name' => 'Turun lentoasema',
+        'image' => 'turku.png',
         'city_population' => '2.000.000'
     ],
     [
-        'city_name' => 'Helsinki',
-        'city_airport' => 'Helsinki-Vantaa',
-        'city_image' => 'city-thumbnail-03.png',
+        'city' => 'Helsinki',
+        'name' => 'Helsinki-Vantaa',
+        'image' => 'helsinki.png',
         'city_population' => '1'
     ]
 ];
