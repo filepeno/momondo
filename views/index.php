12087c10ae5cc7a657f65c87345ebe65c2ed3a5a
filip <fso@stromlin.com>
2022-09-08 13:02:15 +0200

    Add mini logo and render with picture el, add head as require

--- views/index.php
+++ views/index.php
@@ -1,18 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Momondo</title>
-    <link rel="stylesheet" href="index.css">
-
-</head>
+<?php
+$page_title = 'Momondo';
+require_once '../modules/head.php'
+?>
 
 <body>
     <header>
-        <img class="logo" src="assets/img/momondo-logo.png" alt="Momondo logo" width="200">
+        <picture class="logo">
+            <source media="(max-width: 969px)" srcset="assets/img/logo/momondo-logo-sm.png">
+            <source media="(min-width: 970px)" srcset="assets/img/logo/momondo-logo-md.png">
+            <img src="assets/img/logo/momondo-logo-sm.png" alt="Momondo logo" width="125">
+        </picture>
         <div class="links">
             <a class="header-link" href="about-us">About us</a>
             <a class="header-link" href="contact-us">Contact us</a>
