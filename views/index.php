fb5e50e80aee86ad7ceec9fcb8be1495c151fef2
filip <fso@stromlin.com>
2022-09-08 12:27:10 +0200

    Create api login and redirect

--- views/index.php
+++ views/index.php
@@ -17,7 +17,7 @@
             <a class="header-link" href="about-us">About us</a>
             <a class="header-link" href="contact-us">Contact us</a>
             <a class="header-link" href="">Trips</a>
-            <a id="log-in-btn" href="api-login">Log in</a>
+            <a id="log-in-btn" href="login">Log in</a>
             <a href="">English</a>
         </div>
 
