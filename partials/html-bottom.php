e7c191f0d6b86984f19bb0131481637f85a9f687
filip <fso@stromlin.com>
2022-09-12 18:10:42 +0200

    style side header with icons

--- partials/html-bottom.php
+++ partials/html-bottom.php
@@ -1,4 +1,5 @@
 <script src="app.js" type="module"></script>
+<script src="https://code.iconify.design/iconify-icon/1.0.0/iconify-icon.min.js"></script>
 </body>
 
 </html>
--- partials/side-header.php
+++ partials/side-header.php
@@ -2,11 +2,26 @@
     <nav id="side-nav">
         <button id="toggle-side-nav">Toggle</button>
         <ul>
-            <li><a class="side-nav-link active" href=""><span class="link-text">Flights</span></a></li>
-            <li><a class="side-nav-link" href=""><span class="link-text">Flights</span></a></li>
-            <li><a class="side-nav-link" href=""><span class="link-text">Flights</span></a></li>
-            <li><a class="side-nav-link" href=""><span class="link-text">Flights</span></a></li>
+            <a class="side-nav-link <?= $page_href == '/sign-in' ? 'active' : '' ?>" href="/sign-in"><span class="link-text">Sign in</span></a>
         </ul>
+        <ul>
+            <li><a class="side-nav-link <?= $page_href == '/' ? 'active' : '' ?>" href="/"><span class="link-text">Flights</span></a></li>
+            <li><a class="side-nav-link <?= $page_href == '/stays' ? 'active' : '' ?>" href="/stays"><span class="link-text">Stays</span></a></li>
+            <li><a class="side-nav-link <?= $page_href == '/car-rental' ? 'active' : '' ?>" href="/car-rental"><span class="link-text">Car Rental</span></a></li>
+            <li><a class="side-nav-link <?= $page_href == '/things-to-do' ? 'active' : '' ?>" href="/things-to-do"><span class="link-text">Things to do</span></a></li>
+            <li><a class="side-nav-link <?= $page_href == '/packages' ? 'active' : '' ?>" href="/packages"><span class="link-text">Packages</span></a></li>
+            <li><a class="side-nav-link <?= $page_href == '/trains-and-buses' ? 'active' : '' ?>" href="/trains-and-buses"><span class="link-text">Trains and buses</span></a></li>
+        </ul>
+        <ul>
+            <a class="side-nav-link <?= $page_href == '/sign-in' ? 'active' : '' ?>" href="/explore"><span class="link-text">Explore</span></a>
+            <a class="side-nav-link <?= $page_href == '/sign-in' ? 'active' : '' ?>" href="/travel-restrictions"><span class="link-text">Travel Restrictions</span></a>
+        </ul>
+        <ul>
+            <a class="side-nav-link <?= $page_href == '/trips' ? 'active' : '' ?>" href="<?= $page_href ?>"><span class="link-text">Trips</span></a>
+
+        </ul>
+
+
 
     </nav>
 </header>
