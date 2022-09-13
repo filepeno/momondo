2e26cb899a6a7ce1a11e8bbcca7e8a348c1e6b28
filip <fso@stromlin.com>
2022-09-13 17:26:22 +0200

    move js to modules and check side nav class to toggle

--- js/side-nav.js
+++ js/side-nav.js
@@ -1,4 +1,12 @@
-export function toggleSideNav() {
-  //console.log("click", event.target);
-  document.querySelector("#side-nav").classList.toggle("active");
+export function trackSideNav() {
+  document.querySelector("#toggle-menu-btn").addEventListener("click", () => toggleSideNav());
+}
+
+function toggleSideNav() {
+  const nav = document.querySelector("#side-nav");
+  if (nav.classList.contains("active")) {
+    nav.classList.remove("active");
+  } else {
+    nav.classList.add("active");
+  }
 }
