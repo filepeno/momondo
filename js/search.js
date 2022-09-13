c121169e5c4bcef0ff93d3abb7960014f9b86ef4
filip <fso@stromlin.com>
2022-09-13 17:30:40 +0200

    change snake case to camel case

--- js/search.js
+++ js/search.js
@@ -1,58 +1,58 @@
 export function trackSearchInput() {
   document.querySelectorAll(".location-input").forEach((element) => {
-    element.addEventListener("input", (e) => get_input(e));
-    element.addEventListener("focus", (e) => get_input(e));
-    element.addEventListener("blur", (e) => hide_suggestions(e));
+    element.addEventListener("input", (e) => getInput(e));
+    element.addEventListener("focus", (e) => getInput(e));
+    element.addEventListener("blur", (e) => hideSuggestions(e));
   });
 }
 
-async function get_input(e) {
+async function getInput(e) {
   const input = e.target.value.toLowerCase();
   const suggestionsWrapper = e.target.nextElementSibling;
   if (input.length > 0) {
-    const data = await get_cities(input);
-    display_cities(data, suggestionsWrapper, input);
-    display_suggestions(suggestionsWrapper);
+    const data = await getCities(input);
+    displayCities(data, suggestionsWrapper, input);
+    displaySuggestions(suggestionsWrapper);
   } else {
-    hide_suggestions(e);
+    hideSuggestions(e);
   }
 }
 
-function display_suggestions(el) {
-  hide_all_suggestions();
+function displaySuggestions(el) {
+  hideAllSuggestions();
   el.classList.add("active");
 }
 
-function hide_all_suggestions() {
+function hideAllSuggestions() {
   document.querySelectorAll(".suggestions-wrapper").forEach((element) => {
     element.classList.remove("active");
   });
 }
 
-function hide_suggestions(e) {
+function hideSuggestions(e) {
   e.target.nextElementSibling.classList.remove("active");
 }
 
-async function get_cities(input) {
+async function getCities(input) {
   const resp = await fetch("api/api-get-airports.php");
   const data = await resp.json();
-  const filtered_airports = data.filter((airport) => airport.city.toLowerCase().includes(input));
+  const filteredAirports = data.filter((airport) => airport.city.toLowerCase().includes(input));
   console.log(input);
-  return filtered_airports;
+  return filteredAirports;
 }
 
-function display_cities(data, parent, input) {
+function displayCities(data, parent, input) {
   parent.innerHTML = "";
 
   data.forEach((airport) => {
     console.log(airport.city.toLowerCase().indexOf(input));
-    const city_html = `<div class="city">
+    const cityHtml = `<div class="city">
                       <img class="city-img" src="assets/img/${airport.image}" alt="Image of city" width="100px">
                       <div class="city-data-wrapper">
                           <h2 class="city-name">${airport.city ?? "UPS..."}</h2>
                           <p>${airport.name ?? "ups.."}</p>
                       </div>
                   </div>`;
-    parent.insertAdjacentHTML("beforeEnd", city_html);
+    parent.insertAdjacentHTML("beforeEnd", cityHtml);
   });
 }
