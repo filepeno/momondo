c1601fc230147d7983dd033bc1bc46c26e516ae6
filip <fso@stromlin.com>
2022-09-15 10:32:21 +0200

    style log in btn

--- partials/top-header.php
+++ partials/top-header.php
@@ -8,7 +8,7 @@
         <a class="header-link <?= $page_href == '/about-us' ? 'active' : '' ?>" href="/about-us">About us</a>
         <a class="header-link <?= $page_href == '/contact-us' ? 'active' : '' ?>" href="/contact-us">Contact us</a>
         <a class="header-link <?= $page_href == '/trips' ? 'active' : '' ?>" href="/trips">Trips</a>
-        <a id="log-in-btn" href="login">Log in</a>
+        <a class="profile icon white" id="log-in-btn" href="login">Log in</a>
         <a href="">English</a>
     </div>
 </div>
