76e84c0f07170e1c890723276e70aad3042566f5
Filip Soudakov <filips08@gmail.com>
2022-09-28 09:31:09 +0200

    add static reccent searches

--- views/index.php
+++ views/index.php
@@ -43,7 +43,38 @@ require_once '../partials/top-header.php'
         </div>
 
         <div id="bottom-wrapper">
-
+            <div class="recent-searches">
+                <h2 class="section-title h3"><?= $dictionary["{$language}_recent_searches"] ?></h2>
+                <ul class="item-list">
+                    <li>
+                        <a class="recent-search-link" href="">
+                            <img src="/assets/img/helsinki.png" alt="Image of Helsinki" width="60">
+                            <div class="flight-data">
+                                <h3 class="destination">Helsinki (HEL), Finland</h3>
+                                <p class="route">CPH - HEL, 26. okt.</p>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="recent-search-link" href="">
+                            <img src="/assets/img/amsterdam.png" alt="Image of Amsterdam" width="60">
+                            <div class="flight-data">
+                                <h3 class="destination">Amsterdam (AMS), Netherlands</h3>
+                                <p class="route">CPH - AMS, 19. nov.</p>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="recent-search-link" href="">
+                            <img src="/assets/img/london.png" alt="Image of London" width="60">
+                            <div class="flight-data">
+                                <h3 class="destination">London (LON), United Kingdom</h3>
+                                <p class="route">CPH - LON, 22. dec.</p>
+                            </div>
+                        </a>
+                    </li>
+                </ul>
+            </div>
         </div>
     </main>
 </div>
